065747758e9bd0d509dcc595fa4d89b8c70b7829
Pranay More <pranay.m.0799@gmail.com>
2025-07-01 02:03:08 +0100

    Keep only dataflow/pipeline.py in the repo and ignore all other files

--- sql/monthly_spend.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE VIEW customer_analytics.monthly_spend AS
-SELECT
-  customer_id,
-  FORMAT_TIMESTAMP('%Y-%m', timestamp) AS month,
-  COUNT(*) AS transaction_count,
-  SUM(amount) AS total_spend,
-  AVG(amount) AS average_spend
-FROM customer_analytics.transactions
-GROUP BY customer_id, month;
